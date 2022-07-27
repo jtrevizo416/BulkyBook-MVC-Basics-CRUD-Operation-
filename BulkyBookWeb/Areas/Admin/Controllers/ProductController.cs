360cf17c277be30406920635d994b61bed89f55d
jtrevizo416 <jtrevizo416@gmail.com>
2022-07-27 17:07:02 -0500

    Added Product page with Crud functionality

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,145 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers;
+[Area("Admin")]
+
+public class ProductController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _hostEnviroment;
+
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnviroment)
+    {
+        _unitOfWork = unitOfWork;
+        _hostEnviroment = hostEnviroment;
+    }
+
+    public IActionResult Index()
+    {
+        
+        return View();
+    }
+
+   
+    //GET action method
+    public IActionResult Upsert(int? id)
+    {
+        ProductVM productVM = new()
+        {
+            Product = new(),
+            CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+            CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            })
+        };
+
+        if (id == null || id == 0)
+        {
+            //create product
+            //ViewBag.CategoryList = CategoryList;
+            //ViewData["CoverTypeList"] = CoverTypeList;
+            return View(productVM);
+        }
+        else
+        {
+            //update the product
+            productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            return View(productVM);
+        }
+        return View();
+    }
+
+    //POST action method
+    [HttpPost]
+    [ValidateAntiForgeryToken]//search this later in videos for class
+    public IActionResult Upsert(ProductVM obj, IFormFile? imageFile)
+    {
+        if (ModelState.IsValid)
+        {
+            string wwwRootPath = _hostEnviroment.WebRootPath;
+            if (imageFile != null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(wwwRootPath, @"images\products");
+                var extension = Path.GetExtension(imageFile.FileName);
+
+                if (obj.Product.ImageURL != null)
+                {
+                    var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageURL.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                using (var fileStreams = new FileStream(Path.Combine(uploads,fileName+extension), FileMode.Create))
+                {
+                    imageFile.CopyTo(fileStreams);
+                }
+                obj.Product.ImageURL = @"\images\products\" + fileName + extension;
+            }
+
+            if (obj.Product.Id == 0)
+            {
+                _unitOfWork.Product.Add(obj.Product);
+            }
+            else
+            {
+                _unitOfWork.Product.Update(obj.Product);
+            }
+            
+            _unitOfWork.Save();//saves all the changes
+            TempData["success"] = "Product created successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+ 
+    #region API CALLS
+    //API endpoint
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+        return Json(new { data = productList });
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int? id)
+    {
+        var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+
+        if (obj == null)
+        {
+            return Json(new { success = false, message = "Error while deleting" });
+        }
+
+        var oldImagePath = Path.Combine(_hostEnviroment.WebRootPath, obj.ImageURL.TrimStart('\\'));
+        if (System.IO.File.Exists(oldImagePath))
+        {
+            System.IO.File.Delete(oldImagePath);
+        }
+
+        _unitOfWork.Product.Remove(obj);//update object information on the database 
+        _unitOfWork.Save();//saves all the changes
+        return Json(new { success = true, message = "Delete was successfull" });
+       
+    }
+    #endregion
+}
+
+
+
+
